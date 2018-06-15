20a38b64ac0fdd58ccc36a407e5b33d2b8440376
roman <stakhroma@gmail.com>
2018-06-15 13:29:28 +0300

    clean

--- app/src/main/java/com/prospektdev/trainee_stakhovskiy/api/modules/v1/IDogsModule.java
+++ app/src/main/java/com/prospektdev/trainee_stakhovskiy/api/modules/v1/IDogsModule.java
@@ -2,8 +2,6 @@ package com.prospektdev.trainee_stakhovskiy.api.modules.v1;
 
 import com.prospektdev.trainee_stakhovskiy.api.dto.RDog;
 
-import java.util.List;
-
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
